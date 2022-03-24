11140cafa0c56776b56b0b6ff9745089ce733102
Dave Reed <dave.reed@alteryx.com>
2022-03-24 08:33:28 -0400

    release 1.0.0 (#24)

--- setup.py
+++ setup.py
@@ -3,26 +3,26 @@ from os import path
 from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
+with open(path.join(this_directory, "README.md")) as f:
     long_description = f.read()
 
 setup(
-    name='featuretools_sklearn_transformer',
-    version='0.2.0',
-    author='Feature Labs, Inc.',
-    author_email='support@featurelabs.com',
-    license='BSD 3-clause',
-    url='http://www.featurelabs.com/',
-    python_requires='>=3.7, <4',
-    install_requires=open('requirements.txt').readlines(),
+    name="featuretools_sklearn_transformer",
+    version="1.0.0",
+    author="Feature Labs, Inc.",
+    author_email="support@featurelabs.com",
+    license="BSD 3-clause",
+    url="http://www.featurelabs.com/",
+    python_requires=">=3.7, <4",
+    install_requires=open("requirements.txt").readlines(),
     packages=find_packages(),
-    description='Featuretools Transformer for Scikit-Learn Pipeline use.',
+    description="Featuretools Transformer for Scikit-Learn Pipeline use.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     include_package_data=True,
     entry_points={
         "featuretools_plugin": [
-            'wrappers = featuretools_sklearn_transformer',
+            "wrappers = featuretools_sklearn_transformer",
         ],
     },
 )
